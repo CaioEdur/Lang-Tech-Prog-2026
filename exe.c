601c836d30a3cd23af203700808071e0053aacb7
CaioEdur <claudecirtatu2017@gmail.com>
2026-08-12 22:31:11 -0300

    Add files via upload

---
+++ exe.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int primeiro, segundo, AUX;
+	
+	printf("=================EXERCICIO 1=================\n");
+	
+	printf("\nInsira um numero: ");
+	scanf("%d", &primeiro);
+	
+	printf("\nInsira o segundo numero: ");
+	scanf("%d", &segundo);
+	
+	AUX = primeiro;
+	primeiro = segundo; 
+	segundo = AUX;
+	
+	printf("A ordem inversa deles sao %d e %d", primeiro, segundo);
+	
+	printf("\n=============================================\n");
+	
+	printf("=================EXERCICIO 2=================\n");
+	
+	double valor; 
+	
+	printf("\nInsira o valor: ");
+	scanf("%lf", &valor);
+	
+	printf("\nO valor em notacao e:  ", valor);
+	
+	printf("\n=============================================\n");
+	
+	printf("=================EXERCICIO 2=================\n");
+	
+	double valor
+	
+	printf("")
+	
+	
+	return 0;
+}
