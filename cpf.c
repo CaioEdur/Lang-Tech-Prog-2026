ce16fc30e95411695b9c2feefd9cba98d6533e67
CaioEdur <claudecirtatu2017@gmail.com>
2026-09-10 18:39:28 -0300

    Update cpf.c

--- cpf.c
+++ cpf.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main(int argc, char *argv[]) {
 	
-	int c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, soma, soma2;
+	int c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, soma, soma2, valido1, valido2;
 	
 	printf("Informe o cpf: ");
 	scanf("%d %d %d . %d %d %d . %d %d %d - %d %d", &c1, &c2, &c3, &c4, &c5, &c6, &c7, &c8, &c9, &c10, &c11);
 	
+	valido1 = c10;
+	valido2 = c11;
+	
     c1 = c1*10;
-	c1 = c2*9;
-	c1 = c3*8;
-	c1 = c4*7;
-	c1 = c5*6;
-	c1 = c6*5; 
-	c1 = c7*4;
-	c1 = c8*3;
-	c1 = c9*2;
+	c2 = c2*9;
+	c3 = c3*8;
+	c4 = c4*7;
+	c5 = c5*6;
+	c6 = c6*5; 
+	c7 = c7*4;
+	c8 = c8*3;
+	c9 = c9*2;
 	
 	soma = c1 + c2 + c3 + c4 + c5 + c6 + c7 + c8 + c9;
 	
@@ -40,11 +41,13 @@ int main(int argc, char *argv[]) {
 	soma2 = soma2*10;
 	soma2 = soma2%11;
 	
-	if (soma == c10 && soma2 == c11)
-		printf("o cpf e valido");
+	if(soma2%11 == 10){
+		soma2 = 0;
+	}
+	if(soma == valido1 && soma2 == valido2)
+		printf("O cpf e valido");
 	else
-		printf("o cpf e invalido");
-	
-	
+		printf("O cpf nao e valido");
+		
 	return 0;
 }
